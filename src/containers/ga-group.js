2db2385c17b49c2db8f11149e5bc5683653fc2e9
Hugo Costa <13484910+hug0hq@users.noreply.github.com>
2021-08-08 03:10:02 +0100

    🔗 fix links

--- src/containers/ga-group.js
+++ src/containers/ga-group.js
@@ -7,44 +7,44 @@ import { A1, A2, /* A3, A4, A5, A6, */ A7, /* A8, A9, A10 */ } from '../p5'
 export const GaGroup = (props) => {
     return (
         <>
-            <GaCard linkColor="#000" href="https://editor.p5js.org/hug0Hq/sketches/qSd-4t2Wt">
+            <GaCard linkColor="#000" href="https://editor.p5js.org/hug0/sketches/qSd-4t2Wt">
                 <Image className="im" cloudName="hug0" publicId="go-sqrgradient1" crop="scale" />
             </GaCard>
-            <GaCard href="https://editor.p5js.org/hug0Hq/sketches/acMx6vOG4">
+            <GaCard href="https://editor.p5js.org/hug0/sketches/acMx6vOG4">
                 <A1 className="im"></A1>
             </GaCard>
-            <GaCard linkColor="#fff" href="https://editor.p5js.org/hug0Hq/sketches/D7RH0Hm9y">
+            <GaCard linkColor="#fff" href="https://editor.p5js.org/hug0/sketches/D7RH0Hm9y">
                 <A2 className="im"></A2>
             </GaCard>
-            <GaCard linkColor="#f36a19" href="https://editor.p5js.org/hug0Hq/sketches/7b-Me5PCa">
+            <GaCard linkColor="#f36a19" href="https://editor.p5js.org/hug0/sketches/7b-Me5PCa">
                 {/* <A3 className="im"></A3> */}
                 <Image className="im" cloudName="hug0" publicId="ga-randomdotgrid_zkc71d" crop="scale" />
             </GaCard>
-            <GaCard href="https://editor.p5js.org/hug0Hq/sketches/cxPnkj95Q">
+            <GaCard href="https://editor.p5js.org/hug0/sketches/cxPnkj95Q">
                 {/*  <A4 className="im"></A4> */}
                 <Image className="im" cloudName="hug0" publicId="ga-circleshadow" crop="scale" />
             </GaCard>
-            <GaCard href="https://editor.p5js.org/hug0Hq/sketches/PUTOVojk1">
+            <GaCard href="https://editor.p5js.org/hug0/sketches/PUTOVojk1">
                 {/*  <A5 className="im"></A5> */}
                 <Image className="im" cloudName="hug0" publicId="ga-circlesgrid" crop="scale" />
             </GaCard>
-            <GaCard href="https://editor.p5js.org/hug0Hq/sketches/n3X0ZXdxk">
+            <GaCard href="https://editor.p5js.org/hug0/sketches/n3X0ZXdxk">
                 {/* <A6 className="im"></A6> */}
                 <Image className="im" cloudName="hug0" publicId="ga-coloredcirclesgrid" crop="scale" />
             </GaCard>
-            <GaCard linkColor="#f36a19" href="https://editor.p5js.org/hug0Hq/sketches/URwNbuFhB">
+            <GaCard linkColor="#f36a19" href="https://editor.p5js.org/hug0/sketches/URwNbuFhB">
                 {/*  <A10 className="im"></A10> */}
                 <Image className="im" cloudName="hug0" publicId="ga-blackpen" crop="scale" />
             </GaCard>
-            <GaCard linkColor="#f36a19" href="https://editor.p5js.org/hug0Hq/sketches/3E6uv8tyD">
+            <GaCard linkColor="#f36a19" href="https://editor.p5js.org/hug0/sketches/3E6uv8tyD">
                 {/* <A9 className="im"></A9> */}
                 <Image className="im" cloudName="hug0" publicId="ga-squaregridrotation" crop="scale" />
             </GaCard>
-            <GaCard href="https://editor.p5js.org/hug0Hq/sketches/V7SOXVkpw">
+            <GaCard href="https://editor.p5js.org/hug0/sketches/V7SOXVkpw">
                 {/* <A8 className="im"></A8> */}
                 <Image className="im" cloudName="hug0" publicId="ga-squaregrid" crop="scale" />
             </GaCard>
-            <GaCard href="https://editor.p5js.org/hug0Hq/sketches/P4vg5KCxF">
+            <GaCard href="https://editor.p5js.org/hug0/sketches/P4vg5KCxF">
                 <A7 className="im"></A7>
             </GaCard>
         </>
--- src/containers/pens.js
+++ src/containers/pens.js
@@ -5,12 +5,12 @@ import { Pen } from '../components';
 export const Pens = (props) => {
     return (
         <div className="pens">
-            <Pen id="codepen-crain" color="#edda5b" linkColor="#fff" href="https://codepen.io/hug0Hq/pen/KKMVGQg" />
-            <Pen id="codepen-2dgolf" color="#1d1d1b" linkColor="#fff" href="https://codepen.io/hug0Hq/pen/ExyyYNZ" />
-            <Pen id="codepen-amsterdamtext" color="#282c34" linkColor="#fff" href="https://codepen.io/hug0Hq/pen/OJXXLyB" />
-            <Pen id="codepen-kawaiighost" color="#0cbab9" href="https://codepen.io/hug0Hq/pen/NEXgKa" />
-            <Pen id="codepen-cookie" color="#f6f8ff" href="https://codepen.io/hug0Hq/pen/PozNeEQ" />
-            <Pen id="codepen-chick" color="#A9E2F3" href="https://codepen.io/hug0Hq/pen/ZEzPVLP" />
+            <Pen id="codepen-crain" color="#edda5b" linkColor="#fff" href="https://codepen.io/hug0hq/pen/KKMVGQg" />
+            <Pen id="codepen-2dgolf" color="#1d1d1b" linkColor="#fff" href="https://codepen.io/hug0hq/pen/ExyyYNZ" />
+            <Pen id="codepen-amsterdamtext" color="#282c34" linkColor="#fff" href="https://codepen.io/hug0hq/pen/OJXXLyB" />
+            <Pen id="codepen-kawaiighost" color="#0cbab9" href="https://codepen.io/hug0hq/pen/NEXgKa" />
+            <Pen id="codepen-cookie" color="#f6f8ff" href="https://codepen.io/hug0hq/pen/PozNeEQ" />
+            <Pen id="codepen-chick" color="#A9E2F3" href="https://codepen.io/hug0hq/pen/ZEzPVLP" />
         </div>
     );
 }
